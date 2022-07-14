4aa84db4cc99078b17bb8cf0f7279108b26db26a
Morgan <mprior@marstone.com>
2022-07-14 12:39:19 -0400

    fixed return issues, replaced conditionals with match, added documentation

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,5 @@
 use rand::Rng;
+use std::cmp::Ordering;
 
 #[derive(Debug)]
 pub struct Cluster {
@@ -7,111 +8,146 @@ pub struct Cluster {
     delta2: i32,
 }
 
-impl Cluster {
-    fn get_cluster_delta(&self, delta: &str) -> i32 {
-        if delta == "delta2" {
-            return self.delta2; 
-        }
-        else if delta == "delta1" {
-            return self.delta1; 
-        }
-        else {
-            return self.delta0; 
-        }
-    } 
+pub enum Delta {
+    Delta0,
+    Delta1,
+    Delta2,
 }
 
-fn build_cluster(delta0: i32, delta1: i32, delta2: i32) -> Cluster {
-    Cluster{
-        delta0: delta0, 
-        delta1: delta1, 
-        delta2: delta2
+impl Cluster {
+    fn get_cluster_delta(&self, delta: &Delta) -> i32 {
+        
+        //! `get_cluster_delta()` gets the value of the given delta type (0, 1, or 2)
+        //! for a cluster
+
+        match delta {
+            Delta::Delta0 => self.delta0,
+            Delta::Delta1 => self.delta1,
+            Delta::Delta2 => self.delta2,
+        }
     }
 }
 
-pub fn make_fake_cluster_vec()-> Vec<Cluster>{
-    let v: Vec<Cluster> = vec![build_cluster(523, 990, 431), 
-                                build_cluster(371, 499, 212), 
-                                build_cluster(490, 1097, 117), 
-                                build_cluster(242, 947, 098), 
-                                build_cluster(761, 866, 514), 
-                                build_cluster(241, 281, 131), 
-                                build_cluster(520, 824, 378)]; 
+//TODO: Get rid of build_cluster() and make it part of a test
+fn build_cluster(delta0: i32, delta1: i32, delta2: i32) -> Cluster {
+    Cluster {
+        delta0,
+        delta1,
+        delta2,
+    }
+}
+//TODO: Get rid of make_fake_cluster_vec() and make it part of a test
+pub fn make_fake_cluster_vec() -> Vec<Cluster> {
+    let v: Vec<Cluster> = vec![
+        build_cluster(523, 990, 431),
+        build_cluster(371, 499, 212),
+        build_cluster(490, 1097, 117),
+        build_cluster(242, 947, 198),
+        build_cluster(761, 866, 514),
+        build_cluster(241, 281, 131),
+        build_cluster(520, 824, 378),
+    ];
 
     v
 }
 
-pub fn find_kth_delta0(v: &mut Vec<Cluster>, k: usize) -> i32 {
-    let l = 0; 
-    let r = v.len()-1;
-
-    find_kth(v, l, r, k, "delta0")
-}
-
-pub fn find_kth_delta1(v: &mut Vec<Cluster>, k: usize) -> i32 {
-    let l = 0; 
-    let r = v.len()-1;
-
-    find_kth(v, l, r, k, "delta1")
-}
-
-pub fn find_kth_delta2(v: &mut Vec<Cluster>, k: usize) -> i32 {
-    let l = 0; 
-    let r = v.len()-1;
-
-    find_kth(v, l, r, k, "delta2")
-}
-
-pub fn sort_by_delta0(v: &mut Vec<Cluster>) ->  &mut Vec<Cluster> {
+pub fn sort_by_delta0(v: &mut [Cluster]) {
     v.sort_by(|a, b| a.delta0.cmp(&b.delta0));
-    v
 }
-pub fn sort_by_delta1(v: &mut Vec<Cluster>) -> &mut Vec<Cluster> {
+
+pub fn sort_by_delta1(v: &mut [Cluster]) {
     v.sort_by(|a, b| a.delta1.cmp(&b.delta1));
-    v
 }
-pub fn sort_by_delta2(v: &mut Vec<Cluster>) -> &mut Vec<Cluster> {
+
+pub fn sort_by_delta2(v: &mut [Cluster]) {
     v.sort_by(|a, b| a.delta2.cmp(&b.delta2));
-    v
 }
 
+pub fn partition(v: &mut [Cluster], l: usize, r: usize, delta: &Delta) -> usize {
+    //! Called by `random_partition()`. Takes the same arguments as `find_kth()` and returns the "true"
+    //! index (i.e., index if `v` were sorted) of the Cluster currently at the rightmost possible index
+    //! that could still contain the `k`th Cluster (i.e.,`r`th index). Cluster at this `r`th index is based
+    //! on random `pivot` from `random_partition()`.
+    //!
+    //! `j` tracks the index of the element being evaluated at each iteration of the while loop. `i`
+    //! counts the number of elements whose delta value is less than that of the `r`th element.
+    //!
+    //! If a Cluster has a delta value greater than or equal to that of the `r`th element, it  
+    //! swaps position with the next Cluster whose delta is less than that of the `r`th element.
+    //!
+    //! Since `i` counts the number of elements with a delta less than the `r`th element, the final
+    //! swap of the `i`th and `r`th elements puts that `r`th element in its correct position (as if
+    //! `v` were sorted).
 
-pub fn partition(v: &mut Vec<Cluster>, l: usize, r: usize, delta: &str) -> usize {
-    let mut i = l; 
+    let mut i = l;
     let mut j = l;
 
     while j < r {
         if v[j].get_cluster_delta(delta) < v[r].get_cluster_delta(delta) {
             v.swap(i, j);
-            i += 1; }
-            
-        j += 1; }
-    
-    v.swap(i, r);
-    i }
-
-pub fn random_partition(v: &mut Vec<Cluster>, l: usize, r: usize, delta: &str) -> usize {
-    let n = r - l;
-    let pivot = rand::thread_rng().gen_range(0..=(n));
-    v.swap(l+pivot, r); 
-    partition(v, l, r, delta) 
-}
-
-pub fn find_kth(v: &mut Vec<Cluster>, l: usize, r: usize, k: usize, delta: &str) -> i32 {
-    let mut b = v[0].get_cluster_delta(delta); 
-
-    if l <= r {
-        let partition_index = random_partition(v, l, r, delta); 
-
-        if partition_index == k {
-            b = v[partition_index].get_cluster_delta(delta); 
-            return b; 
-        } else if partition_index > k {
-            return find_kth(v, l, partition_index-1, k, delta); 
-        } else if partition_index < k {
-            return find_kth(v, partition_index+1, r, k, delta); 
+            i += 1;
         }
 
+        j += 1;
     }
-    return b; 
+
+    v.swap(i, r);
+    i
+}
+
+pub fn random_partition(v: &mut [Cluster], l: usize, r: usize, delta: &Delta) -> usize {
+    //! Takes the same arguments as `find_kth()` and returns the "true" index (index if `v` were sorted)
+    //! of the Cluster currently at a randomly selected `pivot` index.
+    //!
+    //! Chooses a random `pivot` value within the range plausible indices
+    //! for the `k`th Cluster and swaps the `r`th element with the element in the pivot position.
+    //! Calls `partition()` which evaluates all elements in `v` against the `r`th element (which
+    //! is the element formerly at the `pivot` index).
+    //!
+    //! `partition()` returns a count of elements with a delta value less than that at the `r`th
+    //! (formerly, `pivot`) index (i.e., the index of rightmost element if `v` were sorted); `random_partition()`
+    //! returns this same value
+
+    let pivot = rand::thread_rng().gen_range(l..=r);
+    v.swap(pivot, r);
+    partition(v, l, r, delta)
+}
+
+pub fn find_kth(v: &mut [Cluster], l: usize, r: usize, k: usize, delta: &Delta) -> i32 {
+    //! `find_kth()` finds the kth element in vector without completely sorting it
+    //! Based on the QuickSelect Algorithm found here: https://www.geeksforgeeks.org/quickselect-algorithm/
+    //!
+    //! Takes mutable reference to a vector of Clusters (`v`), the index of the leftmost element in `v` that could
+    //! possibly contain the `k`th cluster (`l`), the index of the rightmost element in `v` that could possibly
+    //! contain the `k`th cluster (`r`), the desired index (`k`), and the desired delta type of the Cluster in the `k`th
+    //! position if `v` were sorted (`delta`).
+    //!
+    //! With each recursive call, a random pivot is chosen and `partition_index` reflects the number of
+    //! Clusters in `v` with a delta value less than that of the Cluster at the random pivot index. If
+    //! `partition_index < k`, `find_kth()` calls itself with `l` adjusted to reflect the new possible indices
+    //! for the `k`th Cluster (any index greater than `partition_index`). If `partition_index < k` `find_kth()` calls
+    //! itself with `r` similarly adjusted.
+    //!
+    //! Recursion ceases when 'partition_index == k', at which point the desired delta value of the `k`th Cluster
+    //! (`kth_delta`) is returned.
+
+    let mut kth_delta = v[0].get_cluster_delta(delta);
+
+    if l <= r {
+        let partition_index = random_partition(v, l, r, delta);
+
+        match partition_index.cmp(&k) {
+            Ordering::Less => {
+                return find_kth(v, partition_index + 1, r, k, delta);
+            }
+            Ordering::Greater => {
+                return find_kth(v, l, partition_index - 1, k, delta);
+            }
+            Ordering::Equal => {
+                kth_delta = v[partition_index].get_cluster_delta(delta);
+                return kth_delta;
+            }
+        }
+    }
+    kth_delta
 }
--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,22 @@
-use partition_non_generic::find_kth_delta0; 
-use partition_non_generic::find_kth_delta1; 
-use partition_non_generic::find_kth_delta2; 
-use partition_non_generic::sort_by_delta0; 
-use partition_non_generic::sort_by_delta1; 
-use partition_non_generic::sort_by_delta2; 
-use partition_non_generic::make_fake_cluster_vec; 
-use partition_non_generic::Cluster; 
+use partition_non_generic::find_kth;
+use partition_non_generic::make_fake_cluster_vec;
+use partition_non_generic::sort_by_delta0;
+use partition_non_generic::sort_by_delta1;
+use partition_non_generic::sort_by_delta2;
+use partition_non_generic::Cluster;
+use partition_non_generic::Delta;
 
 fn main() {
-    let mut v: Vec<Cluster> = make_fake_cluster_vec(); 
-    
-    println!("kth delta0: {}", find_kth_delta0(&mut v, 2));
-    println!("kth delta1: {}", find_kth_delta1(&mut v, 3));
-    println!("kth delta2: {}", find_kth_delta2(&mut v, 4));
-    println!("sorted by delta0: {:?}", sort_by_delta0(&mut v));
-    println!("sorted by delta1: {:?}", sort_by_delta1(&mut v));
-    println!("sorted by delta2: {:?}", sort_by_delta2(&mut v));
+    let mut v: Vec<Cluster> = make_fake_cluster_vec();
+    let r = v.len() - 1;
 
+    println!("kth delta0: {}", find_kth(&mut v, 0, r, 2, &Delta::Delta0));
+    println!("kth delta1: {}", find_kth(&mut v, 0, r, 3, &Delta::Delta1));
+    println!("kth delta2: {}", find_kth(&mut v, 0, r, 4, &Delta::Delta2));
+    sort_by_delta0(&mut v);
+    println!("sorted by delta0: {:?}", v);
+    sort_by_delta1(&mut v);
+    println!("sorted by delta1: {:?}", v);
+    sort_by_delta2(&mut v);
+    println!("sorted by delta2: {:?}", v);
 }
